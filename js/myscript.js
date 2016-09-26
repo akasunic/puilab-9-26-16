8635b938d2e4b09b841257f9d2bb530133965083
akasunic <akasunic@F14PHD5.HCII.CS.CMU.EDU>
2016-09-26 05:08:14 -0400

    putting initial files up

---
+++ js/myscript.js
@@ -0,0 +1,50 @@
+/* 
+This function should add both an image and a reset button
+hints: you will need the method setAttribute for source, Id and addEventListener (click)
+You can make use of document.querySelector. To select by ID, choose #
+Takes as argument a string that indicates which image to show
+*/
+//var showImage = function(which){
+    //hint: start with an if/else clause
+//    if (which == "great"){
+//        var source = "";
+//    }
+//    else if (??){
+//        ??;
+//    }
+    //first, addi image
+//    var pic = document.createElement("img");
+//    pic.setAttribute("src", ???);
+//    pic.setAttribute("id", ???);
+//    pic.addEventListener(????);
+//    document.querySelector(??).appendChild(???;
+//    //use similar steps to add reset button (hint: use function!)
+//    
+//    
+//}
+///*
+//creates and returns a reset button
+//*/
+//var addReset = function () {
+//    var btn = document.createElement(???);
+//    var txt = document.createTextNode(???);
+//    btn.setAttribute("id", ???);
+//    btn.appendChild(???);
+//    btn.addEventListener();//delete imagw on click
+//    return btn;
+//}
+//    
+
+/*
+//removes both the reset button and the pic associated with it
+//*/
+//var deleteImage = function() {
+//    var divId = "#".concat(this.parentElement.id);
+//    var imageDiv = document.querySelector(divId);
+//    //var btnID = ???;
+//    var btn = document.querySelector(btnID);
+//    //var pic = ???
+//    imageDiv.removeChild(btn);
+//    //?? how to remove image as well?
+//    
+//}
